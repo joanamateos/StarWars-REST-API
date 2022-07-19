1ac895380084b7f68a1606a93ff427edbbf75604
Joana Mateos <joanamateos@hotmail.com>
2022-07-19 12:08:31 +0000

    Ejercicio terminado

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planets, Fav_people, Fav_planets
 #from models import Person
 
 app = Flask(__name__)
@@ -38,6 +38,80 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/people', methods=['GET'])
+def get_people():
+    allpeople = People.query.all() #retorna un arreglo de clases
+    allpeople = list(map(lambda elemento: elemento.serialize(), allpeople)) #itero en cada una de las clases y almaceno el resutlado 
+    print (allpeople)
+    return jsonify({"resultado": allpeople})
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    allplanets = Planets.query.all() #retorna un arreglo de clases
+    allplanets = list(map(lambda elemento: elemento.serialize(), allplanets)) #itero en cada una de las clases y almaceno el resutlado 
+    print (allplanets)
+    return jsonify({"resultado": allplanets})
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    allusers = User.query.all() #retorna un arreglo de clases
+    allusers = list(map(lambda elemento: elemento.serialize(), allusers)) #itero en cada una de las clases y almaceno el resutlado 
+    print (allusers)
+    return jsonify({"resultado": allusers})
+
+@app.route('/people/<int:id>', methods=['GET'])
+def get_one_people(id):
+    #bajo un parametro especifico
+    #onepeople = People.query.filter_by(id=id).first()
+    #return jsonify({"resultado": onepeople})
+    #buscar solo por el id
+    onepeople = People.query.get(id).serialize()
+    #onepeople = onepeople.serialize()
+    return jsonify({"resultado": onepeople})
+
+@app.route('/planets/<int:id>', methods=['GET'])
+def get_one_planet(id):
+    #buscar solo por el id
+    oneplanet = Planets.query.get(id)
+    if oneplanet:
+        oneplanet = Planets.query.get(id).serialize()
+        return jsonify({"resultado": oneplanet})
+
+    else:
+        return jsonify({"resultado": "El planeta no existe"})
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def add_fav_people(people_id):
+     onepeople = People.query.get(people_id)
+     if onepeople:
+        new = Fav_people()
+        new.user_id = 1
+        new.people_id = people_id
+        db.session.add(new)
+        db.session.commit()
+
+        return jsonify({"mensaje": "todo salio bien"})
+     else:
+          
+        return jsonify({"mensaje": "error"}) 
+
+
+@app.route('/favorite/planets/<int:planets_id>', methods=['POST'])
+def add_fav_planet(planets_id):
+     oneplanet = Planets.query.get(planets_id)
+     if oneplanet:
+        new = Fav_planets()
+        new.user_id = 1
+        new.planets_id = planets_id
+        db.session.add(new)
+        db.session.commit()
+
+        return jsonify({"mensaje": "todo salio bien"})
+     else:
+          
+        return jsonify({"mensaje": "error"})  
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -9,11 +9,86 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class People (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    gender = db.Column(db.String(80), unique=False, nullable=True)
+    hair_color= db.Column(db.String(80), unique=False, nullable=True)
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+            # do not serialize the password, its a security breach
+        }
+
+class Planets (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+    population = db.Column(db.Integer, unique=False, nullable=True)
+    gravity = db.Column(db.String(80), unique=False, nullable=True)
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "gravity": self.gravity,
+            # do not serialize the password, its a security breach
+        }
+class Fav_people(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+    rel_user = db.relationship(User)
+    rel_people = db.relationship(People)
+
+    def __repr__(self):
+        return '<Fav_people %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "people_id": self.people_id,
+            "rel_user": self.rel_user,
+            "rel_people": self.rel_people,
+            # do not serialize the password, its a security breach
+        }
+
+class Fav_planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+    rel_user = db.relationship(User)
+    rel_planets = db.relationship (Planets)
+
+    def __repr__(self):
+        return '<Fav_planets %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planets_id": self.planets_id,
+            "rel_user": self.rel_user,
+            "rel_planets": self.rel_planets,
             # do not serialize the password, its a security breach
         }
